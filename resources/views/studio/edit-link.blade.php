8889acbf3a322987128f45e18c86825ce3ff6255
aprillio <aprillio@kemenkeu.go.id>
2021-07-06 15:52:07 +0700

    update studio edit link blade

--- resources/views/studio/edit-link.blade.php
+++ resources/views/studio/edit-link.blade.php
@@ -11,13 +11,23 @@
           <input type="text" name="link" value="{{ $link }}" class="form-control" placeholder="https://google.com">
         </div>
         <div class="form-group col-lg-8">
+          <label>Title</label>
+          <input type="text" name="title" value="{{ $title }}" class="form-control" placeholder="Google">
+        </div>
+        <div class="form-group col-lg-8">
           <label for="exampleFormControlSelect1">Button</label>
           <select class="form-control" name="button">
             @foreach($buttons as $button)
-            <option> {{ $button->name }} </option>
+            <option <?= ($buttonId === $button->id) ? 'selected' : '' ?>> {{ $button->name }} </option>
             @endforeach
           </select>
         </div>
+        
+        <div class="form-group col-lg-8">
+          <label>Order</label>
+          <input type="number" name="order" value="{{ $order }}" class="form-control" placeholder="use for ordering links">
+        </div>
+        
         <button type="submit" class="mt-3 ml-3 btn btn-info">Submit</button>
         </form>
 
