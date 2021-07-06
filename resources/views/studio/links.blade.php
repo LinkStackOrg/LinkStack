c5bff254139ece2b2d7366f1b747a137f1e4105e
aprillio <aprillio@kemenkeu.go.id>
2021-07-06 15:52:40 +0700

    update studio links list blade

--- resources/views/studio/links.blade.php
+++ resources/views/studio/links.blade.php
@@ -8,19 +8,23 @@
         <thead>
           <tr>
             <th scope="col">Link</th>
-            <th scope="col">Edit</th>
+            <th scope="col">Title</th>
             <th scope="col">Click</th>
-            <th scope="col">Up Link</th>
+            <th scope="col">Order ⏶</th>
+            <th scope="col">Up Link ⏶</th>
+            <th scope="col">Edit</th>
             <th scope="col">Delete</th>
           </tr>
         </thead>
         <tbody>
         @foreach($links as $link)
           <tr>
-            <td title="{{ $link->link }}">{{ Str::limit($link->link, 30) }}</td>
-            <td><a href="{{ route('editLink', $link->id ) }}">Edit</a></td>
+            <td title="{{ $link->link }}">{{ Str::limit($link->link, 50) }}</td>
+            <td title="{{ $link->title }}">{{ Str::limit($link->title, 50) }}</td>
             <td>{{ $link->click_number }}</td>
+            <td>{{ $link->order }}</td>
             <td><a href="{{ route('upLink', ['up' => $link->up_link, 'id' => $link->id]) }}" class="text-primary">{{ $link->up_link }}</a></td>
+            <td><a href="{{ route('editLink', $link->id ) }}">Edit</a></td>
             <td><a href="{{ route('deleteLink', $link->id ) }}" class="text-danger">Delete</a></td>
           </tr>
           @endforeach
