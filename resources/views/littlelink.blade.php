0750e48830ccad6e431e147bf81e4026a103dec5
JulianPrieber <60265788+JulianPrieber@users.noreply.github.com>
2022-02-12 14:59:07 +0100

    Added support for social media preview
    This shows a preview for Name, Bio and avatar image of users profiles if shared on social sites like Facebook,  Twitter, Facebook Messenger, WhatsApp, Discord, Pinterest and many more.

--- resources/views/littlelink.blade.php
+++ resources/views/littlelink.blade.php
@@ -6,6 +6,35 @@
   <meta name="description" content="{{ $userinfo->littlelink_description }}">
   <meta name="author" content="{{ $userinfo->name }}">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+<!--#### BEGIN Meta Tags social media preview images  ####-->
+  <!-- This shows a preview for title, description and avatar image of users profiles if shared on social media sites -->
+
+    <!-- Facebook Meta Tags -->
+    <meta property="og:url" content="{{ config('app.url') }}/@littlelink_name"">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="{{ $userinfo->littlelink_name }}">
+    <meta property="og:description" content="{{ $userinfo->littlelink_description }}">
+    @if(file_exists(public_path("img/$littlelink_name" . ".png" )))
+    <meta property="og:image" content="{{ asset("img/$littlelink_name" . ".png") }}">
+    @else
+    <meta property="og:image" content="{{ asset('littlelink/images/avatar@2x.png') }}">
+    @endif
+    
+    <!-- Twitter Meta Tags -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta property="twitter:domain" content="{{ config('app.url') }}/@littlelink_name">
+    <meta property="twitter:url" content="https://prieber.com/socials/index.php/">
+    <meta name="twitter:title" content="{{ $userinfo->littlelink_name }}">
+    <meta name="twitter:description" content="{{ $userinfo->littlelink_description }}">
+    @if(file_exists(public_path("img/$littlelink_name" . ".png" )))
+    <meta name="twitter:image" content="{{ asset("img/$littlelink_name" . ".png") }}">
+    @else
+    <meta name="twitter:image" content="{{ asset('littlelink/images/avatar@2x.png') }}">
+    @endif
+
+<!--#### END Meta Tags social media preview images  ####-->
+  
   <link href="//fonts.googleapis.com/css?family=Open+Sans:400,600,800&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="{{ asset('littlelink/css/normalize.css') }}">
   <link rel="stylesheet" href="{{ asset('littlelink/css/skeleton-light.css') }}">
