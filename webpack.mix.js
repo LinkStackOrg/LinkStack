02a4ae7e0c6160a8b191f2c9a8b2fd9c6cb63d50
JulianPrieber <60265788+JulianPrieber@users.noreply.github.com>
2022-02-14 13:28:19 +0100

    Change public path
    This is one of multiple public path edits. For simplified usability, I am integrating the public Laravel directory into the root directory. 
    
    Changed path /public to root directory.
    
    !!!NEEDS TO BE TESTED!!!

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,7 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js').postCss('resources/css/app.css', 'public/css', [
+mix.js('resources/js/app.js', 'js').postCss('resources/css/app.css', 'css', [
     require('postcss-import'),
     require('tailwindcss'),
     require('autoprefixer'),
