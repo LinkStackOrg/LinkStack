0d55159b07f7d4bd1f920fe2e50af51e8c5fed06
JulianPrieber <60265788+JulianPrieber@users.noreply.github.com>
2022-02-20 14:45:34 +0100

    fixed logo.svg

--- resources/views/studio/page.blade.php
+++ resources/views/studio/page.blade.php
@@ -14,11 +14,9 @@
           
           <div class="form-group col-lg-8">
           @if(file_exists(base_path("img/$page->littlelink_name" . ".png" )))
-          <img src="{{ asset("img/$page->littlelink_name" . ".png") }}" srcset="{{ asset("img/$page->littlelink_name" . "@2x.png 2x") }}" width="100px" height="100px">
-          @if(file_exists(base_path("littlelink/images/avatar.png" )))
-          <img class="rounded-avatar" src="{{ asset('littlelink/images/avatar.png') }}" srcset="{{ asset('littlelink/images/avatar@2x.png 2x') }}" width="100px" height="100px">
+          <img src="{{ asset("img/$page->littlelink_name" . ".png") }}" style="width: 75px; border-radius: 50%;">
           @else
-          <img src="{{ asset('littlelink/images/logo.svg') }}" srcset="{{ asset('littlelink/images/avatar@2x.png 2x') }}">
+          <img src="{{ asset('littlelink/images/logo.svg') }}" style="width: 75px;">
           @endif
           </div>
           
