324db451e91a1ecd0032278e272e48bd8b9de46f
JulianPrieber <60265788+JulianPrieber@users.noreply.github.com>
2022-02-20 15:20:57 +0100

    Fixed menu icon
    Fixed issue of the menu hamburger icon not displaying

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -91,11 +91,11 @@
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
           <div class="container-fluid">
             <button type="button" id="sidebarCollapse" class="btn btn-primary">
-              <i class="fa fa-bars"></i>
+              <i class="bi bi-list"></i>
               <span class="sr-only">Toggle Menu</span>
             </button>
             <button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <i class="fa fa-bars"></i>
+                <i class="bi bi-list"></i>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
               <ul class="nav navbar-nav ml-auto">
