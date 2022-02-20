ac741d42e6dbc4613e0d3f0deb1237885965d48a
JulianPrieber <60265788+JulianPrieber@users.noreply.github.com>
2022-02-20 18:15:54 +0100

    custom logo hotfix
    Just a quick fix, the logo wasn't displaying correctly I probably should do this with a separate CSS file for all images, but I just did this quick and dirty with a style attribute

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,5 +1,5 @@
 @if(file_exists(base_path("littlelink/images/avatar.png" )))
-<img class="mb-5" src="{{ asset('littlelink/images/avatar.png') }}" srcset="{{ asset('littlelink/images/avatar@2x.png 2x') }}">
+<img class="mb-5" src="{{ asset('littlelink/images/avatar.png') }}" srcset="{{ asset('littlelink/images/avatar@2x.png 2x') }}" style="width: 150px;">
 @else
 <img class="mb-5" src="{{ asset('littlelink/images/avatar@2x.png') }}">
 @endif
