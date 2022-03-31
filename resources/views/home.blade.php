0aa99064ebd9b6f45860ae4d6f9eacd5d30c8391
Julian Prieber <60265788+JulianPrieber@users.noreply.github.com>
2022-03-31 14:06:58 +0200

    Fixed fadein class for home message

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -91,9 +91,9 @@ foreach($pages as $page)
         <h1 class="mt-5 fadein"> {{ config('app.name') }} </h1>
 
         <!-- Short Bio -->
-        <p class="mt-5 fadein">
+        <div class="mt-5 fadein">
 		      <?php echo $message->home_message; ?>
-		    </p>
+        </div>
         
 <?php $initial=1; // <-- Effectively sets the initial loading time of the buttons. This value should be left at 1. ?>
         <!-- Replace # with your profile URL. Delete whatever you don't need & create your own brand styles in css/brands.css -->  
