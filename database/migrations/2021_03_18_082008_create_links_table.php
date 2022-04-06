559ecbee21806a2dcc447b25ee30ba8da156fd4a
Julian Prieber <julian.prieber@littlelink-custom.com>
2022-04-06 13:58:17 +0200

    Update 2021_03_18_082008_create_links_table.php

--- database/migrations/2021_03_18_082008_create_links_table.php
+++ database/migrations/2021_03_18_082008_create_links_table.php
@@ -25,7 +25,7 @@ class CreateLinksTable extends Migration
             $table->foreign('user_id')->references('id')->on('users');
             $table->foreign('button_id')->references('id')->on('buttons');
             $table->timestamps();
-            $table->text('custom_css');
+            $table->text('custom_css')->default('');
         });
     }
 
