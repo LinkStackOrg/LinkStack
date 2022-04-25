aadd01c4a22ed336a68a9e4e0ab9ea3d8ca76f9d
Julian Prieber <julian.prieber@littlelink-custom.com>
2022-04-25 20:30:25 +0200

    Fixed Integrity constraint violation: 19 UNIQUE

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -33,7 +33,7 @@ class RegisteredUserController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'name' => 'required|string|max:255',
+            'name' => 'required|string|max:255|unique:users',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|confirmed|min:8',
         ]);
