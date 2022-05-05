a65509bd585048cbd3684243518a433d725bc705
Julian Prieber <julian.prieber@littlelink-custom.com>
2022-05-05 15:11:35 +0200

    Update register.blade.php
    Added option to override registration, even if disabled with the setting in the database.
    
    This is something that probably 99.999% of users will never need, but I really need a function like this for my personal instance.
    If "REGISTER_OVERRIDE=true" is added to the .env config file, users will be able to register no matter what is set with the setting in the database. This is useful if two LittleLink Custom instances access the same database, but one should allow registration and the other should not.
    
    If this causes any errors in the future, I will remove this.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -13,7 +13,7 @@ foreach($pages as $page)
                 <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
             </a>
         </x-slot>
-		  @if($page->register == 'true')
+		  @if($page->register == 'true' or env('REGISTER_OVERRIDE') === true)
         <!-- Validation Errors -->
         <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
