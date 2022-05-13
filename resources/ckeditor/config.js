ee37d4a54060d435bb823fad51452e8e7315a5e0
Julian Prieber <60265788+JulianPrieber@users.noreply.github.com>
2022-05-13 17:29:36 +0200

    Added 'justify text' option to text editor

--- resources/ckeditor/config.js
+++ resources/ckeditor/config.js
@@ -35,4 +35,6 @@ CKEDITOR.editorConfig = function( config ) {
 
 	// Simplify the dialog windows.
 	config.removeDialogTabs = 'image:advanced;link:advanced';
+
+	config.extraPlugins = 'justify';
 };
