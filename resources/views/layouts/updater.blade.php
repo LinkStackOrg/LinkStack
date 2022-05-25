1fd71f17277fcce87b45caa20ea31bdc0b25580b
Julian Prieber <60265788+JulianPrieber@users.noreply.github.com>
2022-05-25 16:22:14 +0200

    Update updater.blade.php

--- resources/views/layouts/updater.blade.php
+++ resources/views/layouts/updater.blade.php
@@ -4,6 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Update</title>
   <link href="//fonts.googleapis.com/css?family=Open+Sans:400,600,800&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="{{ asset('littlelink/css/normalize.css') }}">
   <link rel="stylesheet" href="{{ asset('littlelink/css/brands.css') }}">
