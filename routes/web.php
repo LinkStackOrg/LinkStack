c90e49fa1c0c6187293b7cbff679e98d4925b571
Julian Prieber <60265788+JulianPrieber@users.noreply.github.com>
2022-05-25 20:53:00 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -103,7 +103,7 @@ Route::get('/updating', function (\Codedge\Updater\UpdaterManager $updater) {
       unlink(base_path("backups/CANUPDATE"));
 
       $URL = Route::current()->getName();   
-      header("Location: ".$URL."/../update?success");
+      header("Location: ".$URL."/../update?finishing");
       exit(); 
 
   } else {
