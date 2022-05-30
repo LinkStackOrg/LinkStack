b445eb85ffc187177cbe768282e98c90231ccb93
Julian Prieber <60265788+JulianPrieber@users.noreply.github.com>
2022-05-30 23:10:08 +0200

    Re-implemented footer hover animations

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,16 +1,16 @@
 <div class="container">
 <div class="footer fadein" style="margin:5% 0px 35px 0px;">
 @if(env('DISPLAY_FOOTER') === true)
-    <a class="hvr-float spacing" href="{{ url('') }}/">Home</a>
-    <a class="hvr-float spacing" href="{{ url('') }}/pages/terms">Terms</a>
-    <a class="hvr-float spacing" href="{{ url('') }}/pages/privacy">Privacy</a>
-    <a class="hvr-float spacing" href="{{ url('') }}/pages/contact">Contact</a>
+    <a class="footer-hover spacing" href="{{ url('') }}/">Home</a>
+    <a class="footer-hover spacing" href="{{ url('') }}/pages/terms">Terms</a>
+    <a class="footer-hover spacing" href="{{ url('') }}/pages/privacy">Privacy</a>
+    <a class="footer-hover spacing" href="{{ url('') }}/pages/contact">Contact</a>
 @endif
 </div>
 
 @if(env('DISPLAY_CREDIT') === true)
 <div class="credit-footer"><a style="text-decoration: none;" class="spacing" href="https://littlelink-custom.com" target="_blank" title="Learn more">
-	<section class="hvr-grow fadein">
+	<section class="credit-hover hvr-grow fadein">
 		<div class="parent-footer credit-icon" >
 			<img id="footer_spin" class="footer_spin image-footer1 generic" src="{{ asset('littlelink/images/just-gear.svg') }}" alt="LittleLink Custom"></img>
 			<img class="image-footer2" src="{{ asset('littlelink/images/just-ll.svg') }}" alt="LittleLink Custom"></img>
