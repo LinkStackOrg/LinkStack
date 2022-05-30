5b73de758498691faa9cd41c4cfae997aaf28a82
Julian Prieber <60265788+JulianPrieber@users.noreply.github.com>
2022-05-30 10:50:01 +0200

    Update update.blade.php

--- resources/views/update.blade.php
+++ resources/views/update.blade.php
@@ -4,10 +4,7 @@
 <div class="container">
 
 <?php // Requests newest version from server and sets it as variable
-					ini_set('user_agent', 'Mozilla/4.0 (compatible; MSIE 6.0)');
-					$json = file_get_contents("https://api.github.com/repos/julianprieber/littlelink-custom/releases/latest") ;
-					$myObj = json_decode($json);
-				  $Vgit = $myObj->tag_name; 
+			   		$Vgit = file_get_contents("https://julianprieber.github.io/littlelink-custom/version.json"); 
 
 				       // Requests current version from the local version file and sets it as variable
                   $Vlocal = 'v' . file_get_contents(base_path("version.json")); 
