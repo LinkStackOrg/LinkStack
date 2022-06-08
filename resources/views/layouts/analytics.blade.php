75d7e845671ab6390c7bff89af99dae854641c6b
Julian Prieber <60265788+JulianPrieber@users.noreply.github.com>
2022-06-08 16:38:52 +0200

    Updated analytics layout

--- resources/views/layouts/analytics.blade.php
+++ resources/views/layouts/analytics.blade.php
@@ -1,7 +1,14 @@
-<!-- Analytics -->
 <?php
 $analyticsHTML = Config::get('meta.analytics');
 $analyticsHTML = preg_replace("~<!--(.*?)-->~s", "", $analyticsHTML);
-echo $analyticsHTML;
+$analyticsHTML = preg_replace('/[ \t]+/', ' ', preg_replace('/\s\S*$^\s\S*/m', "\n", $analyticsHTML));
+$analyticsHTML = trim($analyticsHTML);
 ?>
+
+@if(preg_replace( "/\r|\n/", "", $analyticsHTML ) != '')
+<!-- Analytics -->
+
+{!! $analyticsHTML !!}
+
 <!-- /Analytics -->
+@endif
