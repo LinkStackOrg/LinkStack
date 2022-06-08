8639c44fa83a814acbd611e62e6be6cd03cbfc13
Julian Prieber <60265788+JulianPrieber@users.noreply.github.com>
2022-06-08 16:45:03 +0200

    Fixed formatting bug

--- resources/views/layouts/analytics.blade.php
+++ resources/views/layouts/analytics.blade.php
@@ -1,7 +1,6 @@
 <?php
 $analyticsHTML = Config::get('meta.analytics');
 $analyticsHTML = preg_replace("~<!--(.*?)-->~s", "", $analyticsHTML);
-$analyticsHTML = preg_replace('/[ \t]+/', ' ', preg_replace('/\s\S*$^\s\S*/m', "\n", $analyticsHTML));
 $analyticsHTML = trim($analyticsHTML);
 ?>
 
