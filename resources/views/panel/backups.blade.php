0ad7c44b3de22223f6b46aa0a2788ea9b1f249a9
Julian Prieber <60265788+JulianPrieber@users.noreply.github.com>
2022-06-16 13:52:56 +0200

    Update backups.blade.php

--- resources/views/panel/backups.blade.php
+++ resources/views/panel/backups.blade.php
@@ -4,12 +4,12 @@
 <style>.buttondm{display:inline-block;text-decoration:none;height:48px;text-align:center;vertical-align:middle;font-size:18px;width:300px;font-weight:700;line-height:48px;letter-spacing:.1px;white-space:wrap;border-radius:8px;cursor:pointer}.button-hover,.credit-hover{display:inline-block;-webkit-transform:perspective(1px) translateZ(0);transform:perspective(1px) translateZ(0);box-shadow:0 0 1px rgba(0,0,0,0);-webkit-transition-duration:.3s;transition-duration:.3s;-webkit-transition-property:transform;transition-property:transform}.button-hover:active,.credit-hover:active,.button-hover:focus,.credit-hover:focus,.button-hover:hover,.credit-hover:hover{-webkit-transform:scale(1.06);transform:scale(1.06)}.container{align-items:center;display:flex;flex-direction:column;justify-content:center;height:50%;width:100%}</style>
   <!-- Custom icons font-awesome -->
   <script src="https://kit.fontawesome.com/c4a5e06183.js" crossorigin="anonymous"></script>
-  @if (file_exists(base_path('storage/updater-backups/')) and is_dir(base_path('storage/updater-backups/')))
+  @if (file_exists(base_path('backups/updater-backups/')) and is_dir(base_path('backups/updater-backups/')))
     @if($_SERVER['QUERY_STRING'] != '')
     <?php
     $filename = $_SERVER['QUERY_STRING'];
     
-    $filepath = base_path('storage/updater-backups/') . $filename;
+    $filepath = base_path('backups/updater-backups/') . $filename;
     
     $strFile = file_get_contents($filepath);			
     
