2102d88373b4b69ebb03dba440256c8b05a342b9
Julian Prieber <60265788+JulianPrieber@users.noreply.github.com>
2022-07-05 00:03:25 +0200

    Update advanced-config-update-1.php

--- storage/templates/advanced-config-update-1.php
+++ storage/templates/advanced-config-update-1.php
@@ -1,14 +1,16 @@
 
 
-/*
+    /*
     |--------------------------------------------------------------------------
     | Extra Settings
     |--------------------------------------------------------------------------
     |
     */
 
-    'expand_panel_admin_menu_permanently' => 'false',
+    // Expands admin menu on the Admin Panel Sidebar permanently
+    'expand_panel_admin_menu_permanently' => 'false', // Either "true" or "false".
 
-    'disable_default_password_notice' => 'false',
+    // Should only be used in a local testing environment
+    'disable_default_password_notice' => 'false', // Either "true" or "false".
 
 ];
