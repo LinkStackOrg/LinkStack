9f8b865a35e3948e4d44859eb07d1de648f66b7e
Julian Prieber <60265788+JulianPrieber@users.noreply.github.com>
2022-09-09 22:28:11 +0200

    Added theme "Stargazer"

---
+++ themes/stargazer/config.php
@@ -0,0 +1,54 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Theme Config
+    |--------------------------------------------------------------------------
+    |
+    | Description.
+    |
+    */
+
+    // Some themes may not be compatible with custom buttons created by the Button Editor.
+    // If 'false' the default button CSS is used.
+    'allow_custom_buttons' => 'false',
+
+    'open_links_in_same_tab' => 'false',
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Code
+    |--------------------------------------------------------------------------
+    |
+    | Description.
+    |
+    */
+
+    'enable_custom_code' => 'false',
+
+    // Disable individual files (only applies if above is 'true').
+    'enable_custom_head'     => 'true',
+    'enable_custom_body'     => 'true',
+    'enable_custom_body_end' => 'true',
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Icons
+    |--------------------------------------------------------------------------
+    |
+    | Description.
+    |
+    */
+
+    'use_custom_icons' => 'false',
+
+    // Is not set correct this will cause errors.
+    'custom_icon_extension' => '.svg', // (.png, .jpg ...)
+
+
+
+];
---
+++ themes/stargazer/readme.md
@@ -0,0 +1,26 @@
+# A LittleLink Custom Theme
+Find more themes: https://github.com/JulianPrieber/llc-themes
+                                                                                                                                                                         
+*	Theme Name: Stargazer
+*	Theme Version: 1.1
+*	Theme Date: 2022-09-01
+*	Theme Author: JulianPrieber
+*	Theme Author URI: https://github.com/JulianPrieber
+*	Theme License: GPLv3
+
+## Preview:
+![Preview](https://i.imgur.com/S84rKl4.gif)
+
+
+### Used assets:
+* Built using:
+* https://github.com/dhg/Skeleton
+* License: MIT
+
+* Built using:
+* https://codepen.io/SuneBear/pen/YzVgebG
+* License: MIT
+
+* Built using:
+* https://codepen.io/trishalim/pen/WNEOdWK
+* License: MIT
