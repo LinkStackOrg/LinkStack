bc82dd4cbcdaa0bfde739f918280603c306ef8d8
Adam Tommasi <4682372+ATommasi@users.noreply.github.com>
2022-09-19 10:55:42 -0400

    Fix missing logo path in blade component

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,5 +1,5 @@
-@if(file_exists(base_path("/images/avatar.png" )))
-<img class="mb-5" src="{{ asset('/images/avatar.png') }}" srcset="{{ asset('/images/avatar@2x.png 2x') }}" style="width: 150px;">
+@if(file_exists(base_path("/content/images/avatar.png" )))
+<img class="mb-5" src="{{ asset('/content/images/avatar.png') }}" srcset="{{ asset('/content/images/avatar@2x.png 2x') }}" style="width: 150px;">
 @else
-<img class="mb-5" src="{{ asset('/images/avatar@2x.png') }}">
+<img class="mb-5" src="{{ asset('/content/images/avatar@2x.png') }}">
 @endif
