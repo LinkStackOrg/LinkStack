8e8d72f9a326eafdd6c26d1107ea3fac5b892449
Julian Prieber <60265788+JulianPrieber@users.noreply.github.com>
2022-11-09 17:33:22 +0100

    Update self-update.php

--- config/self-update.php
+++ config/self-update.php
@@ -6,7 +6,7 @@ declare(strict_types=1);
 if (env('JOIN_BETA') == true) {
     $userver = 'https://beta.littlelink-custom.com/';
 } else {
-    $userver = 'https://update.littlelink-custom.com/';
+    $userver = 'https://updates.littlelink-custom.com/';
 }
 
 return [
