69a5139b70ddac045ce2d2dfa96c23b381d686cf
Julian Prieber <60265788+JulianPrieber@users.noreply.github.com>
2022-11-11 19:11:15 +0100

    Update index.blade.php

--- resources/views/panel/index.blade.php
+++ resources/views/panel/index.blade.php
@@ -1,25 +1,52 @@
 @extends('layouts.sidebar')
 
+
 @section('content')
 @if($littlelink_name == '')
-        <h2 class="mb-4"> 👋 Hi, stranger</h2>
+        <h3 class="mb-4"> 👋 Hi, stranger</h3>
         <h5>You do not have a Page URL set, yet you can change that on the <a href="{{ url('/studio/page') }}">Page section</a></h5>
 	@else
-        <h2 class="mb-4"> 👋 Hi, @<?= $littlelink_name ?></h2>
+        <h3 class="mb-4"> 👋 Hi, @<?= $littlelink_name ?></h2>
 		@endif
         <p>
           Welcome to {{ config('app.name') }}!
         </p>
 
-        <div class="mt-5 row">
-          <h5 class="mb-4" title="all links"><i class="bi bi-share-fill"> {{ $siteLinks }} </i></h5>
-          <h5 class="mb-4 ml-5" title="all clicks"><i class="bi bi-eye-fill"> {{ $siteClicks }} </i></h5>
-          <h5 class="mb-4 ml-5" title="all Users"><i class="bi bi bi-person-fill"> {{ $userNumber }}</i></h5>
-        </div>
 
-        <div class="mt-5 row">
-        <h5 class="mb-4"><i class="bi bi-link"> link: {{ $links }} </i></h5>
-          <h5 class="mb-4 ml-5"><i class="bi bi-eye"> click: {{ $clicks }} </i></h5>
-        </div>
+        <!-- Section: Design Block -->
+        <section class="mb-3 text-gray-800 text-center shadow p-4 w-full">
+            <div class='font-weight-bold text-left'>Visitor analytics:</div>
+            <div class="d-flex flex-wrap justify-content-around">
 
-@endsection
+                <div class="p-2">
+                    <h3 class="text-primary"><strong><i class="bi bi-share-fill"> {{ $siteLinks }} </i></strong></h3>
+                    <span class="text-muted">All links</span>
+                </div>
+
+                <div class="p-2">
+                    <h3 class="text-primary"><strong><i class="bi bi-eye-fill"> {{ $siteClicks }} </i></strong></h3>
+                    <span class="text-muted">All clicks</span>
+                </div>
+
+                <div class="p-2">
+                    <h3 class="text-primary"><strong><i class="bi bi bi-person-fill"> {{ $userNumber }}</i></strong></h3>
+                    <span class="text-muted">All users</span>
+                </div>
+
+            </div>
+        </section>
+
+        <section class="mb-3 text-center shadow p-4 w-full">
+            <div class=" d-flex">
+
+                <div class='p-2 h6'><i class="bi bi-link"> Links: {{ $links }} </i></span></div>
+
+                <div class='p-2 h6'><i class="bi bi-eye"> Click: {{ $clicks }} </i></span></div>
+            </div>
+        </section>
+
+
+
+        {{-- <pre>{{ print_r($pageStats) }}</pre> --}}
+
+        @endsection
