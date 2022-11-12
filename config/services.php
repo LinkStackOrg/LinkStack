186af086b742b70ff41abe81b486c3b57d028dce
Julian Prieber <60265788+JulianPrieber@users.noreply.github.com>
2022-11-12 14:02:22 +0100

    Update services.php

--- config/services.php
+++ config/services.php
@@ -1,5 +1,10 @@
 <?php
 
+    // Social login
+    $facebook = url('/social-auth/facebook/callback');
+    $twitter = url('/social-auth/twitter/callback');
+    $google = url('/social-auth/google/callback');
+
 return [
 
     /*
@@ -29,20 +34,21 @@ return [
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
+
     'facebook' => [
         'client_id'     => env('FACEBOOK_CLIENT_ID'),
         'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
-        'redirect'      => env('FACEBOOK_CALLBACK_URL'),
+        'redirect'      => env('FACEBOOK_CALLBACK_URL', $facebook),
     ],
     'twitter' => [
         'client_id'     => env('TWITTER_CLIENT_ID'),
         'client_secret' => env('TWITTER_CLIENT_SECRET'),
-        'redirect'      => env('TWITTER_CALLBACK_URL'),
+        'redirect'      => env('TWITTER_CALLBACK_URL', $twitter),
     ],
     'google' => [
         'client_id'     => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect'      => env('GOOGLE_CALLBACK_URL'),
+        'redirect'      => env('GOOGLE_CALLBACK_URL', $google),
     ],
 
 ];
