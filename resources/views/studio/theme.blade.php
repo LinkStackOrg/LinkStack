1287fae1f4ec94ad701cae3052e5c59e4d67c7bb
Julian Prieber <60265788+JulianPrieber@users.noreply.github.com>
2022-11-25 17:41:00 +0100

    Update theme.blade.php

--- resources/views/studio/theme.blade.php
+++ resources/views/studio/theme.blade.php
@@ -30,14 +30,14 @@
                   }
                     if($page->theme != $entry){echo '<option value="'; print_r($entry); echo '">'; echo $themeName; echo '</option>'; }}}} ?>
                     <?php 
-                    if($page->theme != "default"){
+                    if($page->theme != "default" and $page->theme != ""){
                     if(file_exists(base_path('themes') . '/' . $page->theme . '/readme.md')){
                     $text = file_get_contents(base_path('themes') . '/' . $page->theme . '/readme.md');
                     $pattern = '/Theme Name:.*/';
                     preg_match($pattern, $text, $matches, PREG_OFFSET_CAPTURE);
                     $themeName = substr($matches[0][0],12);}
                     echo '<option value="'.$page->theme.'" selected>'.$themeName.'</option>';} ?>
-                    <?php echo '<option value="default"'; if($page->theme == "default"){echo 'selected';} echo '>Default</option>'; ?>
+                    <?php echo '<option value="default"'; if($page->theme == "default" or $page->theme == ""){echo 'selected';} echo '>Default</option>'; ?>
             </select>
         </div>
         <div class="col">
