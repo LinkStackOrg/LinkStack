2d66578e11ec4b3a56b443b5b9ee09a42210ab49
Julian Prieber <60265788+JulianPrieber@users.noreply.github.com>
2022-11-28 17:02:55 +0100

    Revert "Update button-editor.blade.php"
    This reverts commit 5cc9c1736e01c2408c3df534e3e6af5156132373.

--- resources/views/studio/button-editor.blade.php
+++ resources/views/studio/button-editor.blade.php
@@ -216,7 +216,7 @@ var button_css = {
 @if($buttonId == 1)
 <center><div id="sample" style="--delay: 1s; border-radius:8px !important; max-width: 400px; width: 80%; class="button-entrance"><div class="button-demo button hvr-grow hvr-icon-wobble-vertical"><img class="icon hvr-icon" src="{{ asset('\/littlelink/icons\/') . 'custom' }}.svg">{{ $title }}</div></div></center>
 @else
-<center><div id="sample" style="--delay: 1s; border-radius:8px !important; max-width: 400px; width: 80%; class="button-entrance"><div class="button-demo button hvr-grow hvr-icon-wobble-vertical"><img class="wicon hvr-icon" src="{{asset('studio/favicon/favicon.gif')}}" onload="this.src='{{url('ico').'?'.$id}}'" loading="lazy">{{ $title }}</div></div></center>
+<center><div id="sample" style="--delay: 1s; border-radius:8px !important; max-width: 400px; width: 80%; class="button-entrance"><div class="button-demo button hvr-grow hvr-icon-wobble-vertical"><img class="wicon hvr-icon" src="{{asset('studio/favicon/favicon.gif')}}" onload="this.src='{{url('ico').'?'.$link}}'" loading="lazy">{{ $title }}</div></div></center>
 @endif
 
 </section>
@@ -255,9 +255,9 @@ NULL
       @if($custom_css === "" or $custom_css === "NULL" and $buttonId == 1)
         <center><div style="--delay: 1s" class="button-entrance"><div class="button-demo button-custom button hvr-grow hvr-icon-wobble-vertical"><img class="icon hvr-icon fa {{$custom_icon}}">{{ $title }}</div></div></center>
         @elseif($custom_css === "" or $custom_css === "NULL" and $buttonId == 2)
-        <center><div style="--delay: 1s" class="button-entrance"><div class="button-custom_website button hvr-grow hvr-icon-wobble-vertical"><img class="wicon hvr-icon" src="{{asset('studio/favicon/favicon.gif')}}" onload="this.src='{{url('ico').'?'.$id}}'" loading="lazy">{{ $title }}</div></div></center>
+        <center><div style="--delay: 1s" class="button-entrance"><div class="button-custom_website button hvr-grow hvr-icon-wobble-vertical"><img class="wicon hvr-icon" src="{{asset('studio/favicon/favicon.gif')}}" onload="this.src='{{url('ico').'?'.$link}}'" loading="lazy">{{ $title }}</div></div></center>
         @elseif($custom_css != "" and $buttonId == 2)
-        <center><div style="--delay: 1s" class="button-entrance"><div style="{{ $custom_css }}" class="button-custom_website button hvr-grow hvr-icon-wobble-vertical"><img class="wicon hvr-icon" src="{{asset('studio/favicon/favicon.gif')}}" onload="this.src='{{url('ico').'?'.$id}}'" loading="lazy">{{ $title }}</div></div></center>
+        <center><div style="--delay: 1s" class="button-entrance"><div style="{{ $custom_css }}" class="button-custom_website button hvr-grow hvr-icon-wobble-vertical"><img class="wicon hvr-icon" src="{{asset('studio/favicon/favicon.gif')}}" onload="this.src='{{url('ico').'?'.$link}}'" loading="lazy">{{ $title }}</div></div></center>
         @else
         <center><div style="--delay: 1s" class="button-entrance"><div style="{{ $custom_css }}" class="button-demo hvr-grow hvr-icon-wobble-vertical"><i style="color: {{$custom_icon}}" class="icon hvr-icon fa {{$custom_icon}}"></i>{{ $title }}</div></div></center>
       @endif
