75a5dafe9b69a72539d4149694c11631bc338598
Julian Prieber <60265788+JulianPrieber@users.noreply.github.com>
2022-11-28 17:03:11 +0100

    Revert "Update link-display.blade.php"
    This reverts commit 6064ea026bc68a2d1c02f54741bfc04e8090567d.

--- resources/views/components/pageitems/link-display.blade.php
+++ resources/views/components/pageitems/link-display.blade.php
@@ -5,7 +5,7 @@
 
 <a class="button button-custom button button-hover icon-hover" rel="noopener noreferrer nofollow" href="{{ route('clickNumber') . '/' . $link->id . '/' . $link->link}}" @if(theme('open_links_in_same_tab') !="true" )target="_blank" @endif>
     @if($params->GetSiteIcon ?? true)
-        <img alt="button-icon" class="icon hvr-icon" src="{{asset('studio/favicon/favicon.gif')}}" onload="this.src='{{url('ico').'?'.$link->id}}'" loading="lazy">
+        <img alt="button-icon" class="icon hvr-icon" src="{{asset('studio/favicon/favicon.gif')}}" onload="this.src='{{url('ico').'?'.$BaseURL}}'" loading="lazy">
         @endif
 
     {{ $link->title }}
