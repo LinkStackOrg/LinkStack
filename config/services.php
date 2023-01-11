6273400ddbc2d5ccb7c581ea3a679533fd877c20
Julian Prieber <60265788+JulianPrieber@users.noreply.github.com>
2023-01-11 14:31:24 +0100

    Update services.php
    Added GitHub

--- config/services.php
+++ config/services.php
@@ -45,5 +45,10 @@ return [
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
         'redirect'      => env('GOOGLE_CALLBACK_URL'),
     ],
+    'github' => [
+        'client_id' => env('GITHUB_CLIENT_ID'),
+        'client_secret' => env('GITHUB_CLIENT_SECRET'),
+        'redirect' => 'http://example.com/callback-url',
+    ],
 
 ];
