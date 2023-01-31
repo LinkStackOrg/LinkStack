2329d8345be518e78d06e5390492f96b27408994
Julian Prieber <julian.prieber@llc.ovh>
2023-01-31 15:49:56 +0100

    Added new helper function
    Added new helper function to replace file_get_contents with curl.

---
+++ app/Functions/externalfiles.php
@@ -0,0 +1,13 @@
+<?php
+
+function external_file_get_contents($url) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:80.0) Gecko/20100101 Firefox/80.0');
+    $data = curl_exec($ch);
+    curl_close($ch);
+    return $data;
+}
