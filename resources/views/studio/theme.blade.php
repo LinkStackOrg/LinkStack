98ec150d4ea600a6578afb8a61c05df034afd134
Julian Prieber <julian.prieber@llc.ovh>
2023-03-01 15:15:37 +0100

    Bugfix

--- resources/views/studio/theme.blade.php
+++ resources/views/studio/theme.blade.php
@@ -278,6 +278,7 @@ try{ if($GLOBALS['updateAv'] == true) echo '<img style="padding-left:40px; paddi
 
 @endif
 
+@if(env('ALLOW_CUSTOM_BACKGROUNDS') == true)
 <br><br><br>
 <form action="{{ route('themeBackground') }}" enctype="multipart/form-data" method="post">
     @csrf
@@ -290,8 +291,9 @@ try{ if($GLOBALS['updateAv'] == true) echo '<img style="padding-left:40px; paddi
     </div>
     <div class="form-group col-lg-8">
         @if(!file_exists(base_path('/img/background-img/'.findBackground(Auth::user()->id))))<p><i>No image selected</i></p>@endif
-        <img style="width:95%;max-width:400px;argin-left:1rem!important;border-radius:5px;" src="@if(file_exists(base_path('/img/background-img/'.findBackground(Auth::user()->id)))){{url('/img/background-img/'.findBackground(Auth::user()->id))}}@else{{url('/littlelink/images/themes/no-preview.png')}}@endif">
-        <br><button class="mt-3 ml-3 btn btn-primary" style="background-color:tomato!important;border-color:tomato!important;transform: scale(.9);" title="Delete background image"><a href="{{ url('/studio/rem-background') }}" style="color:#FFFFFF;"><i class="bi bi-trash-fill"></i> Remove background</a></button><br><br>
+        <img style="width:95%;max-width:400px;argin-left:1rem!important;border-radius:5px;" src="@if(file_exists(base_path('/img/background-img/'.findBackground(Auth::user()->id)))){{url('/img/background-img/'.findBackground(Auth::user()->id))}}@else{{url('/littlelink/images/themes/no-preview.png')}}@endif"><br>
+        @if(file_exists(base_path('/img/background-img/'.findBackground(Auth::user()->id))))<button class="mt-3 ml-3 btn btn-primary" style="background-color:tomato!important;border-color:tomato!important;transform: scale(.9);" title="Delete background image"><a href="{{ url('/studio/rem-background') }}" style="color:#FFFFFF;"><i class="bi bi-trash-fill"></i> Remove background</a></button><br>@endif
+        <br>
         <label>Upload background image</label>
         <input type="file" accept="image/jpeg,image/jpg,image/png" class="form-control-file" name="image">
     </div>
@@ -299,6 +301,7 @@ try{ if($GLOBALS['updateAv'] == true) echo '<img style="padding-left:40px; paddi
         <button type="submit" class="mt-3 ml-3 btn btn-secondary">Upload background</button>
     </div>
 </form>
+@endif
 
 <br><br><br>
 <form action="{{ route('editTheme') }}" enctype="multipart/form-data" method="post">
