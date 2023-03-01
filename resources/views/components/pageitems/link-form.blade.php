5e33cdb6760b3345e6655700fd5041096cd9c5ee
Julian Prieber <julian.prieber@llc.ovh>
2023-03-01 16:04:43 +0100

    Fixed savingwithout filling URL
    https://github.com/JulianPrieber/littlelink-custom/issues/338

--- resources/views/components/pageitems/link-form.blade.php
+++ resources/views/components/pageitems/link-form.blade.php
@@ -2,7 +2,7 @@
 <input type='text' name='title' value='{{$link_title}}' class='form-control' required />
 
 <label for='title' class='form-label'>URL</label>
-<input type='url' name='link' value='{{$link_url}}' class='form-control' />
+<input type='url' name='link' value='{{$link_url}}' class='form-control' required />
 
 <div class="custom-control custom-checkbox m-2">
     <input type="checkbox" class="custom-control-input" value='1' {{((isset($params->GetSiteIcon) ? boolval($params->GetSiteIcon) : false) ? 'checked': '') }} name='GetSiteIcon' id="GetSiteIcon" @if($button_id == 2)checked @endif>
