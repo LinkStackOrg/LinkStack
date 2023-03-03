acd9a60b3c88642928e7776c09a52e0898397305
Julian Prieber <julian.prieber@llc.ovh>
2023-03-03 19:07:48 +0100

    Added register link to login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -98,6 +98,7 @@
             </div>
 
             <div class="flex items-center justify-end mt-4">
+                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('register') }}">Register</a>&emsp;
                 @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
