6d9ffdff56a0542fa3fd3746d88c1091b1206932
Julian Prieber <julian.prieber@llc.ovh>
2023-03-09 13:18:42 +0100

    Create 2023_03_09_121613_update_columns_to_text_type.php
    https://github.com/JulianPrieber/littlelink-custom/issues/328

---
+++ database/migrations/2023_03_09_121613_update_columns_to_text_type.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateColumnsToTextType extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->text('littlelink_description')->change();
+        });
+        
+        Schema::table('links', function (Blueprint $table) {
+            $table->text('title')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('littlelink_description', 255)->change();
+        });
+
+        Schema::table('links', function (Blueprint $table) {
+            $table->string('title', 255)->change();
+        });
+    }
+}
