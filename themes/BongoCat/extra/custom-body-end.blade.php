a4344079451cad65db5eef10023e9b85ff4a7d2a
Julian Prieber <julian.prieber@llc.ovh>
2023-04-26 16:01:53 +0200

    Removed themes

--- themes/BongoCat/extra/custom-body-end.blade.php
+++
@@ -1,3 +0,0 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/gsap.min.js"></script>
-<script src="{{themeAsset('script.js')}}"></script>
-</div>
--- themes/polygon/extra/custom-body-end.blade.php
+++
@@ -1,7 +0,0 @@
-<style> /* This is a comment */ </style>
-
-<script> // This is a comment </script>
-
-{{-- This is a comment --}}
-
-<?php // This is a comment ?>
