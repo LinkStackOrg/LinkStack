9f8a5050d3617f6db3b3d04c5817aa8ba02f9d2e
Julian Prieber <julian.prieber@llc.ovh>
2023-05-01 12:49:41 +0200

    Update pre-update.blade.php

--- resources/views/components/pre-update.blade.php
+++ resources/views/components/pre-update.blade.php
@@ -1,1 +1,4 @@
-{{-- Runs before updating --}}
+<?php
+// Runs before updating
+
+if(file_get_contents(base_path("version.json")) < '4.0.0')copy(base_path('storage/templates/advanced-config.php'), base_path('config/advanced-config.php')); 
