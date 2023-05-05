e74d92d25c1f364f17d413a77365d4c294e6abf4
Julian Prieber <julian.prieber@llc.ovh>
2023-05-05 14:43:17 +0200

    Update pre-update.blade.php

--- resources/views/components/pre-update.blade.php
+++ resources/views/components/pre-update.blade.php
@@ -1,4 +1,10 @@
-<?php
-// Runs before updating
-
-if(trim(file_get_contents(base_path("version.json"))) < '4.0.0')file_put_contents(base_path('config/advanced-config.php'), file_get_contents(base_path('storage/templates/advanced-config.php')));
+<?php // Runs before updating
+if(trim(file_get_contents(base_path("version.json"))) < '4.0.0'){
+  try {
+    $file = base_path('storage/RSTAC');
+    if (!file_exists($file)) {
+        $handleFile = fopen($file, 'w');
+        fclose($handleFile);
+    }
+} catch (Exception $e) {}
+}
