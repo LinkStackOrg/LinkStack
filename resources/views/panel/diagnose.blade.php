596798893c182f0a7160338639a83d9fa768b423
Frank <phoenix@dunkleallianz.net>
2023-06-14 16:59:49 +0200

    Translated Diagnose View Panel

--- resources/views/panel/diagnose.blade.php
+++ resources/views/panel/diagnose.blade.php
@@ -5,12 +5,12 @@
 <?php
 use Illuminate\Support\Facades\Http;
 
-$wtrue = "<td style=\"text-align: center; cursor: help;\" title=\"Everything is working as expected!\">✔️</td>";
-$wfalse = "<td style=\"text-align: center; cursor: help;\" title=\"This file cannot be written to. This may impede proper operation.\">❌</td>";
+$wtrue = "<td style=\"text-align: center; cursor: help;\" title=\"{{__('messages.wtrue')}}\">✔️</td>";
+$wfalse = "<td style=\"text-align: center; cursor: help;\" title=\"{{__('messages.wfalse')}}\">❌</td>";
 
-$utrue = "<td style=\"text-align: center; cursor: help;\" title=\"Your security is at risk. This file can be accessed by everyone. Immediate action is required!\">❗</td>";
-$ufalse = "<td style=\"text-align: center; cursor: help;\" title=\"Everything is working as expected!\">✔️</td>";
-$unull = "<td style=\"text-align: center; cursor: help;\" title=\"Something went wrong. This might be normal if you're running behind a proxy or docker container.\">➖</td>";
+$utrue = "<td style=\"text-align: center; cursor: help;\" title=\"{{__('messages.utrue')}}\">❗</td>";
+$ufalse = "<td style=\"text-align: center; cursor: help;\" title=\"{{__('messages.ufalse')}}\">✔️</td>";
+$unull = "<td style=\"text-align: center; cursor: help;\" title=\"{{__('messages.unull')}}\">➖</td>";
 
 
 $server = $_SERVER['SERVER_NAME'];
@@ -55,20 +55,20 @@ $url2 = getUrlSatusCode($actual_link . '/../../database/database.sqlite');
 
 ?>
 
-        <h2 class="mb-4"><i class="bi bi-braces-asterisk"> Debugging information</i></h2>
+        <h2 class="mb-4"><i class="bi bi-braces-asterisk">{{__('messages.Debugging information')}}</i></h2>
 
         @if($url1 == '200' or $url2 == '200')
-        <a href="https://docs.linkstack.org/d/installation-requirements/" target="_blank"><h4 style="color:tomato;">Your security is at risk. Some files can be accessed by everyone. Immediate action is required! <br> Click this message to learn more.</h4></a>
+        <a href="https://docs.linkstack.org/d/installation-requirements/" target="_blank"><h4 style="color:tomato;">{{__('messages.security.risk')}}</h4></a>
         @endif
 
         <h3 class="mb-4">Write access</h3>
-        <p>Here, you can easily verify if important system files can be written to. This is important for every function to work properly. Entries marked with a '✔️' work as expected, entries marked with a '❌' do not.</p>
+        <p>{{__('messages.Write access.description.1-3')}} '✔️' {{__('messages.Write access.description.2-3')}} '❌' {{__('messages.Write access.description.3-3')}}</p>
 
         <table class="table table-bordered">
         <thead>
           <tr>
-            <th scope="col" style="width: 90%;">File</th>
-            <th title="You can hover over entries to learn more about their current status" style="cursor: help;" scope="col">Hover for more</th>
+            <th scope="col" style="width: 90%;">{{__('messages.File')}}</th>
+            <th title="You can hover over entries to learn more about their current status" style="cursor: help;" scope="col">{{__('messages.Hover for more')}}</th>
           </tr>
         </thead>
         <tbody>
@@ -83,14 +83,14 @@ $url2 = getUrlSatusCode($actual_link . '/../../database/database.sqlite');
         </tbody>
         </table>
 
-        <br><h3 class="mb-4">Security</h3>
-        <p>Here, you can easily verify if critical system files can be accessed externally. It is important that these files cannot be accessed, otherwise user data like passwords could get leaked. Entries marked with a '✔️' cannot be accessed externally, entries marked with a '❗' can be accessed by anyone and require immediate action to protect your data.</p>
+        <br><h3 class="mb-4">{{__('messages.Security')}}</h3>
+        <p>{{__('messages.security.risk.1-3')}} '✔️' {{__('messages.security.risk.2-3')}} '❗' {{__('messages.security.risk.3-3')}}</p>
 
         <table class="table table-bordered">
         <thead>
           <tr>
-            <th scope="col" style="width: 90%;">Link</th>
-            <th title="You can hover over entries to learn more about their current status" style="cursor: help;" scope="col">Hover for more</th>
+            <th scope="col" style="width: 90%;">{{__('messages.Link')}}</th>
+            <th title="You can hover over entries to learn more about their current status" style="cursor: help;" scope="col">{{__('messages.Hover for more')}}</th>
 
           </tr>
         </thead>
