1e089214d4b5e294cb0f04eb2f79a4ff3f460f03
Julian Prieber <60265788+JulianPrieber@users.noreply.github.com>
2023-06-17 01:12:58 +0200

    Update site.blade.php

--- resources/views/panel/site.blade.php
+++ resources/views/panel/site.blade.php
@@ -15,25 +15,25 @@
                     <div class="col-sm-12">  
   
                       <section class="text-gray-400">
-                        <h2 class="mb-4 card-header"><i class="bi bi-person"> Site Customization</i></h2>
+                        <h2 class="mb-4 card-header"><i class="bi bi-person"> {{__('messages.Site Customization')}}</i></h2>
                                 <div class="card-body p-0 p-md-3">
                           
                           <form action="{{ route('editSite') }}" enctype="multipart/form-data" method="post">
                           @csrf
                             <div class="form-group col-lg-8">
-                              <label>Site logo</label>@if(file_exists(base_path("assets/linkstack/images/").findFile('avatar')))<a title="Remove icon" class="hvr-grow p-1 text-danger" style="padding-left:5px;" href="{{ route('delAvatar') }}"><i class="bi bi-trash-fill"></i></a>@endif
+                              <label>{{__('messages.Site logo')}}</label>@if(file_exists(base_path("assets/linkstack/images/").findFile('avatar')))<a title="Remove icon" class="hvr-grow p-1 text-danger" style="padding-left:5px;" href="{{ route('delAvatar') }}"><i class="bi bi-trash-fill"></i></a>@endif
                               <div class="mb-3">
                                 <input type="file" class="form-control form-control-lg" name="image" aria-label="Large file input example">
                             </div>
                             </div>
                             <div class="form-group col-lg-8">
-                              <label>Favicon</label>@if(file_exists(base_path("assets/linkstack/images/").findFile('favicon')))<a title="Remove icon" class="hvr-grow p-1 text-danger" style="padding-left:5px;" href="{{ route('delFavicon') }}"><i class="bi bi-trash-fill"></i></a>@endif
+                              <label>{{__('messages.Favicon')}}</label>@if(file_exists(base_path("assets/linkstack/images/").findFile('favicon')))<a title="Remove icon" class="hvr-grow p-1 text-danger" style="padding-left:5px;" href="{{ route('delFavicon') }}"><i class="bi bi-trash-fill"></i></a>@endif
                               <div class="mb-3">
                                 <input type="file" class="form-control form-control-lg" name="icon" aria-label="Large file input example">
                             </div><br>
                             </div>
                             <div class="form-group col-lg-8">
-                              <h3>Home message</h3>
+                              <h3>{{__('messages.Home message')}}</h3>
                               <textarea class="form-control ckeditor" name="message" rows="3">{{ $home_message }}</textarea>
                             </div>
                             <button type="submit" class="mt-3 ml-3 btn btn-primary">Save</button>
