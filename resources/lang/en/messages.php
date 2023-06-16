a7f598ff8f5652b26625bfdeb4c2ca0b9b5f9a9c
Julian Prieber <60265788+JulianPrieber@users.noreply.github.com>
2023-06-16 23:01:46 +0200

    Merged translation files

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -1087,4 +1087,40 @@ return [
     'support.msg6' => 'Thank you for your support and for being a part of the LinkStack community!',
 
 
+    #=============================================================================#
+    # Laravel internal translations                                               #
+    #=============================================================================#
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'These credentials do not match our records.',
+    'password' => 'The provided password is incorrect.',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Pagination Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the paginator library to build
+    | the simple pagination links. You are free to change them to anything
+    | you want to customize your views to better match your application.
+    |
+    */
+
+    'previous' => '&laquo; Previous',
+    'next' => 'Next &raquo;',
+
+
 ];
