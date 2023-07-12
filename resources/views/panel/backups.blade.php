123018697720b97fbbb067dd8ee400ed9d9aad10
Julian Prieber <60265788+JulianPrieber@users.noreply.github.com>
2023-07-12 15:17:55 +0200

    Fixed allowed memory size exhausted error

--- resources/views/panel/backups.blade.php
+++ resources/views/panel/backups.blade.php
@@ -2,24 +2,29 @@
 
 @section('content')
   @if (file_exists(base_path('backups/updater-backups/')) and is_dir(base_path('backups/updater-backups/')))
-    <?php
-    $filename = $_SERVER['QUERY_STRING'];
-    
-    $filepath = base_path('backups/updater-backups/') . $filename;
-    
-    $strFile = file_get_contents($filepath);			
-    
-      header("Content-type: application/force-download");
-      header('Content-Disposition: attachment; filename="'.$filename.'"');	
-      
-      header('Content-Length: ' . filesize($filepath));	
-      echo $strFile;
-      while (ob_get_level()) {
-    	ob_end_clean();
+  <?php
+  $filename = $_SERVER['QUERY_STRING'];
+  $filepath = base_path('backups/updater-backups/') . $filename;
+  
+  if (file_exists($filepath)) {
+      header("Content-type: application/octet-stream");
+      header('Content-Disposition: attachment; filename="' . $filename . '"');
+      header('Content-Length: ' . filesize($filepath));
+  
+      $file = fopen($filepath, 'rb');
+      if ($file) {
+          while (!feof($file)) {
+              echo fread($file, 8192);
+              ob_flush();
+              flush();
+          }
+          fclose($file);
       }
-      readfile($filepath);	 
-    	exit;
-    ?>
+      exit;
+  } else {
+      echo 'File not found.';
+  }
+  ?> 
   @endif
   
 @endsection
