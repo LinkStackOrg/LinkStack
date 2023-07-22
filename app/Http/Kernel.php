c5970309b40a6c11c3d6b58f72f64e4fa740448c
Julian Prieber <60265788+JulianPrieber@users.noreply.github.com>
2023-07-22 13:16:28 +0200

    Revert "Disabled request lifecycle middleware on public routes"
    This reverts commit 658617c8bb2574a241f5384ea18857828eb6a690.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -29,15 +29,13 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middlewareGroups = [
-        'AuthSession' => [
+        'web' => [
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
+            // \Illuminate\Session\Middleware\AuthenticateSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
-        ],
-
-        'web' => [
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
 
@@ -45,7 +43,6 @@ class Kernel extends HttpKernel
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
-
     ];
 
     /**
