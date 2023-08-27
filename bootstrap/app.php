cf388ddfa7561892e794ae79c29ff34f414f3d99
Julian Prieber <60265788+JulianPrieber@users.noreply.github.com>
2023-08-27 15:54:52 +0200

    Fixed php artisan serve

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,10 @@ $app = new Illuminate\Foundation\Application(
     $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
 );
 
+$app->bind('path.public', function () {
+    return dirname(__DIR__);
+});
+
 /*
 |--------------------------------------------------------------------------
 | Bind Important Interfaces
--- server.php
+++ server.php
@@ -18,4 +18,4 @@ if ($uri !== '/' && file_exists(__DIR__.$uri)) {
     return false;
 }
 
-require_once __DIR__.'index.php';
+require_once __DIR__.'/index.php';
