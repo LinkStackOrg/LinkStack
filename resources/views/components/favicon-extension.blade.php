3609d1477a177be973b39d695d77a95ec7fcacd7
Julian Prieber <60265788+JulianPrieber@users.noreply.github.com>
2023-11-23 15:18:51 +0100

    Update favicon-extension.blade.php

--- resources/views/components/favicon-extension.blade.php
+++ resources/views/components/favicon-extension.blade.php
@@ -1,14 +1,18 @@
 <?php
 
-function localIcon($id) {
-  $directory = base_path("assets/favicon/icons");
-  $files = scandir($directory);
-  $pathinfo = "error.error";
-  foreach($files as $file) {
-  if (strpos($file, $id.'.') !== false) {
-  $pathinfo = $id. "." . pathinfo($file, PATHINFO_EXTENSION);
-  }}
-  return $pathinfo;
+if (!function_exists('localIcon')) {
+    function localIcon($id)
+    {
+        $directory = base_path("assets/favicon/icons");
+        $files = scandir($directory);
+        $pathinfo = "error.error";
+        foreach ($files as $file) {
+            if (strpos($file, $id . '.') !== false) {
+                $pathinfo = $id . "." . pathinfo($file, PATHINFO_EXTENSION);
+            }
+        }
+        return $pathinfo;
+    }
 }
   
 ?>
