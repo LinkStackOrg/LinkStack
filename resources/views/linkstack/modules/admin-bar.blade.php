62ed4644eb0aa2dca4d485c7b7394bf508138013
Julian Prieber <60265788+JulianPrieber@users.noreply.github.com>
2023-12-13 16:10:09 +0100

    Update admin-bar.blade.php

--- resources/views/linkstack/modules/admin-bar.blade.php
+++ resources/views/linkstack/modules/admin-bar.blade.php
@@ -48,17 +48,19 @@ if(Auth::user()->id == $userinfo->id){
                 <span class="screen-reader-text">{{__('messages.Home')}}</span></a>
             </li>
             <li id="linkstack-admin-bar-defaultsp">
-              <a class="ab-item" href="{{url('/studio/links')}}">
+              <a class="ab-item" href="@if($isUser){{url('/studio/links')}}@else {{ route('editUser', $userinfo->id ) }}@endif">
                 <svg style="top:3px;position:relative;fill:white;" xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"><path d="M362.7 19.3L314.3 67.7 444.3 197.7l48.4-48.4c25-25 25-65.5 0-90.5L453.3 19.3c-25-25-65.5-25-90.5 0zm-71 71L58.6 323.5c-10.4 10.4-18 23.3-22.2 37.4L1 481.2C-1.5 489.7 .8 498.8 7 505s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L421.7 220.3 291.7 90.3z"/></svg>
                 <span class="ab-label">{{__('messages.Edit')}}</span>
                 <span class="screen-reader-text">{{__('messages.Edit')}}</span></a>
             </li>
+            @if($isUser)
             <li id="linkstack-admin-bar-defaultsp">
               <a class="ab-item" href="{{url('/studio/add-link')}}">
                 <svg style="top:3px;position:relative;fill:white;" xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512"><path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"/></svg>
                 <span class="ab-label">{{__('messages.Add Link')}}</span>
                 <span class="screen-reader-text">{{__('messages.Add Link')}}</span></a>
             </li>
+            @endif
               <li id="linkstack-admin-bar-defaultsp" class="menupop">
                  <a class="ab-item" aria-haspopup="true" href="#">
                   <svg style="top:4px;position:relative;fill:white;" xmlns="http://www.w3.org/2000/svg" height="16" width="14" viewBox="0 0 448 512"><path d="M201.4 342.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 274.7 86.6 137.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"/></svg>
