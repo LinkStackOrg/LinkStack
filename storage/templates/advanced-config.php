e686ba4421b7c6782545536789210c90275d9b64
Julian Prieber <60265788+JulianPrieber@users.noreply.github.com>
2023-12-15 16:16:15 +0100

    Update advanced-config.php

--- storage/templates/advanced-config.php
+++ storage/templates/advanced-config.php
@@ -8,7 +8,7 @@ $analytics =
 |--------------------------------------------------------------------------
 |
 | Add external analytics services to your LinkStack instance by adding them below.
-| Everything you enter below will be added to the <head> tag of every page.
+| Everything you enter below will be added to the <head> tag of every page.
 | Formatting in plain HTML is expected.
 |
 */
@@ -33,7 +33,7 @@ return [
     |
     | Will only be active if "CUSTOM_META_TAGS" is set to "true" in the config.
     | These tags will only be applied to the home page or if a LinkStack page 
-    | is set as the homepage in the config (for example: HOME_URL="admin").
+    | is set as the homepage in the config (for example: HOME_URL="admin").
     | 
     | Empty entries will be ignored.
     |
@@ -43,7 +43,7 @@ return [
     'description'     => '', // Overrides the default meta page description. Leave empty to use your LinkStack page description as the description.
     'robots'          => 'index,follow',
     'viewport'        => 'width=device-width, initial-scale=1',
-    'canonical_url'   => '', // Tells search engines to index "https://example.com/"  instead of "https://example.com/@admin",  for example.
+    'canonical_url'   => '', // Tells search engines to index "https://example.com/" instead of "https://example.com/@admin", for example.
     'twitter_creator' => '', // Twitter @username.
     'author'          => '', // Your name.
 
@@ -123,19 +123,19 @@ return [
     |         'custom_css' => ''
     |       ),
     | 
-    | In the 'button' field, you have to enter the button name (i.e. 'mastodon', 'github', 'custom'...).
+    | In the 'button' field, you have to enter the button name (i.e. 'mastodon', 'github', 'custom'...).
     | You can find a list of all available buttons below.
     | 
-    | In the 'link' field, you can enter your desired link you may leave this field empty for a display only, non-functional button.
+    | In the 'link' field, you can enter your desired link you may leave this field empty for a display only, non-functional button.
     | 
-    | In the 'title' field, changes the text on a button, such as 'custom' and 'custom_website'.
+    | In the 'title' field, changes the text on a button, such as 'custom' and 'custom_website'.
     | 
-    | In the 'icon' field, uses the same syntax as the Button Editor on the Admin Panel. 
+    | In the 'icon' field, uses the same syntax as the Button Editor on the Admin Panel. 
     | This allows you to add your own icons to 'custom' buttons. You can find a list of available icons on linkstack.org/fa.
     | 
-    | In the 'custom_css' field, here you can enter custom CSS to change the color of your button. 
+    | In the 'custom_css' field, here you can enter custom CSS to change the color of your button. 
     | If you don't feel comfortable working with CSS,
-    | you can copy and paste the CSS from the 'Custom CSS' field of the Button Editor on the Admin Panel.
+    | you can copy and paste the CSS from the 'Custom CSS' field of the Button Editor on the Admin Panel.
     |
     */
 
