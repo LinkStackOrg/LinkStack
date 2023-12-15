28b073df2f9540427c4427634320f9501e6a2eaf
Julian Prieber <60265788+JulianPrieber@users.noreply.github.com>
2023-12-15 17:31:28 +0100

    Now counting clicks using JS

--- resources/views/linkstack/elements/icons.blade.php
+++ resources/views/linkstack/elements/icons.blade.php
@@ -3,7 +3,7 @@
         @if(count($icons) > 0)
         <div class="row fadein social-icon-div">
         @foreach($icons as $icon)
-        <a class="social-hover social-link" href="{{ route('clickNumber') . '/' . $icon->id. "?" . $icon->link}}" title="{{ucfirst($icon->title)}}" aria-label="{{ucfirst($icon->title)}}" @if(theme('open_links_in_same_tab') != "true")target="_blank"@endif><i class="dynamic-contrast social-icon fa-brands fa-{{$icon->title}}"></i></a>
+        <a class="social-hover social-link" href="{{ $icon->link }}" title="{{ucfirst($icon->title)}}" aria-label="{{ucfirst($icon->title)}}" @if(theme('open_links_in_same_tab') != "true")target="_blank"@endif><i id="{{ $icon->id }}" class="button-click dynamic-contrast social-icon fa-brands fa-{{$icon->title}}"></i></a>
         @endforeach
         </div>
         @endif
