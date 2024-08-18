ebe36ce917e3bd4004829d48a22141b2de587f06
PaperGram <145454517+papergram@users.noreply.github.com>
2024-08-18 11:57:01 +0200

    fix: typo container in html

--- resources/views/studio/links.blade.php
+++ resources/views/studio/links.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<?php use App\Models\Button; 
+<?php use App\Models\Button;
 
 // Check if the LinkCount cookie is set
 if (isset($_COOKIE['LinkCount'])) {
@@ -47,27 +47,27 @@ if (isset($_COOKIE['LinkCount'])) {
 
 <?php if(!function_exists('strp')){function strp($urlStrp){return str_replace(array('http://', 'https://'), '', $urlStrp);}} ?>
 
-<div class="conatiner-fluid content-inner mt-n5 py-0">
-    <div class="row">   
+<div class="container-fluid content-inner mt-n5 py-0">
+    <div class="row">
 
         <div class="col-lg-12">
             <div class="card   rounded">
                <div class="card-body">
                   <div class="row">
-                      <div class="col-sm-12">  
-    
+                      <div class="col-sm-12">
+
                         <div class="row">
                             <section class='pre-left text-gray-400'>
                                 <h3 class="card-header mb-3"><i class="bi bi-link-45deg">{{__('messages.My Links')}}</i>
                                         <a class="btn btn-primary float-end" href="{{ url('/studio/add-link') }}"> {{__('messages.Add new Link')}}</a>
                                 </h3>
-                            
+
                                 <div>
-                            
+
                                     {{-- <div style="text-align: right;"><a href="{{ url('/studio/links') }}/10">10</a> | <a href="{{ url('/studio/links') }}/20">20</a> | <a href="{{ url('/studio/links') }}/30">30</a> | <a href="{{ url('/studio/links') }}/all">all</a></div> --}}
-                            
+
                                 <div style="overflow-y: none;" class="col col-md-7 ms-3">
-                            
+
                                     <div id="links-table-body" data-page="{{request('page', 1)}}" data-per-page="{{$pagePage ? $pagePage : 0}}">
                                         @if($links->total() == 0)
                                               <div class="col-6 text-center">
@@ -80,16 +80,16 @@ if (isset($_COOKIE['LinkCount'])) {
                                         @if($button->name !== 'icon')
                                         <div class='row h-100 pb-0 mb-2 border rounded hvr-glow w-100' data-id="{{$link->id}}">
                                             <div class="d-flex ">
-                            
-                            
+
+
                                                 <div class='col-auto p-2 my-auto mr-2' title="{{ $link->link }}">
                                                     <svg xmlns="http://www.w3.org/2000/svg" class="sortable-handle" viewBox="0 0 16 16">
                                                         <path d="M1 4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V4zm5 0a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V4zm5 0a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1V4zM1 9a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V9zm5 0a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V9zm5 0a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1V9z"/>
                                                       </svg>
                                                 </div>
-                            
+
                                             <div class='col h-100'>
-                            
+
                                                 <div class='row h-100'>
                                                     <div class='col-12 p-2' style="max-width:300px;overflow:hidden;white-space:nowrap;text-overflow:ellipsis;" title="{{ $link->title }}">
                                                         <span class='h6'>
@@ -106,41 +106,41 @@ if (isset($_COOKIE['LinkCount'])) {
                                                             @else
                                                             <span class="bg-soft-secondary" style="border: 1px solid #d0d4d7 !important;border-radius:5px;width:25px!important;height:25px!important;"><img style="max-width:15px !important;" alt="button-icon" height="15" class="m-1 " src="{{ asset('\/assets/linkstack/icons\/') . $buttonName }}.svg "></span>
                                                             @endif
-                            
+
                                                             {{strip_tags($link->title,'')}}</span>
-                            
+
                                                         @if(!empty($link->link) and $button->name != "vcard")
                                                         <br>
                                                         <a title='{{$link->link}}' href="{{ $link->link}}" target="_blank" class="d-none d-md-block ml-4 text-muted small">{{Str::limit($link->link, 75 )}}</a>
                                                         <a title='{{$link->link}}' href="{{ $link->link}}" target="_blank" class="d-md-none ml-4 text-muted small">{{Str::limit($link->link, 25 )}}</a>
                                                         @elseif(!empty($link->link) and $button->name == "vcard")
                                                         <br><a href="{{ url('vcard/'.$link->id) }}" target="_blank" class="ml-4 small">{{__('messages.Download')}}</a>
-                            
+
                                                         @endif
-                            
+
                                                     </div>
-                            
+
                                                     <div class='col' class="text-right">
                                                         {{Str::limit($link->params['text'] ?? null, 150)  }}
-                            
+
                                                         @if($link->typename == 'video')
                                                             @php
                                                                 $embed = OEmbed::get($link->link);
                                                                 if ($embed && $embed->hasThumbnail()) {
                                                                     echo "<img style='max-height: 150px;' src='".$embed->thumbnailUrl()."' />";
-                            
+
                                                                 }
                                                             @endphp
-                            
+
                                                         @endif
                                                     </div>
-                            
-                            
+
+
                                                     <div class='col-12 py-1 px-3 m-0 mt-2'>
-                            
+
                                                         @if(!empty($link->link))
                                                         <span><i class="bi bi-bar-chart-line"></i> {{ $link->click_number }} {{__('messages.Clicks')}}</span>
-                            
+
                                                         @endif
 
                                                         <a style="float: right;" href="{{ route('deleteLink', $link->id ) }}" onclick="return confirm('{{ __('messages.confirm_delete', ['title' => addslashes($link->title)]) }}')" class="btn btn-sm me-1 btn-icon btn-danger" data-bs-toggle="tooltip" data-bs-placement="top" aria-label="Delete" data-bs-placement="top" data-original-title="{{__('messages.Delete')}}">
@@ -169,7 +169,7 @@ if (isset($_COOKIE['LinkCount'])) {
                                                                     <span class="btn-inner">
                                                                         <svg class="icon-20" width="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="currentColor">
                                                                           <path fill-rule="evenodd" clip-rule="evenodd" d="M20.8064 7.62361L20.184 6.54352C19.6574 5.6296 18.4905 5.31432 17.5753 5.83872V5.83872C17.1397 6.09534 16.6198 6.16815 16.1305 6.04109C15.6411 5.91402 15.2224 5.59752 14.9666 5.16137C14.8021 4.88415 14.7137 4.56839 14.7103 4.24604V4.24604C14.7251 3.72922 14.5302 3.2284 14.1698 2.85767C13.8094 2.48694 13.3143 2.27786 12.7973 2.27808H11.5433C11.0367 2.27807 10.5511 2.47991 10.1938 2.83895C9.83644 3.19798 9.63693 3.68459 9.63937 4.19112V4.19112C9.62435 5.23693 8.77224 6.07681 7.72632 6.0767C7.40397 6.07336 7.08821 5.98494 6.81099 5.82041V5.82041C5.89582 5.29601 4.72887 5.61129 4.20229 6.52522L3.5341 7.62361C3.00817 8.53639 3.31916 9.70261 4.22975 10.2323V10.2323C4.82166 10.574 5.18629 11.2056 5.18629 11.8891C5.18629 12.5725 4.82166 13.2041 4.22975 13.5458V13.5458C3.32031 14.0719 3.00898 15.2353 3.5341 16.1454V16.1454L4.16568 17.2346C4.4124 17.6798 4.82636 18.0083 5.31595 18.1474C5.80554 18.2866 6.3304 18.2249 6.77438 17.976V17.976C7.21084 17.7213 7.73094 17.6516 8.2191 17.7822C8.70725 17.9128 9.12299 18.233 9.37392 18.6717C9.53845 18.9489 9.62686 19.2646 9.63021 19.587V19.587C9.63021 20.6435 10.4867 21.5 11.5433 21.5H12.7973C13.8502 21.5001 14.7053 20.6491 14.7103 19.5962V19.5962C14.7079 19.088 14.9086 18.6 15.2679 18.2407C15.6272 17.8814 16.1152 17.6807 16.6233 17.6831C16.9449 17.6917 17.2594 17.7798 17.5387 17.9394V17.9394C18.4515 18.4653 19.6177 18.1544 20.1474 17.2438V17.2438L20.8064 16.1454C21.0615 15.7075 21.1315 15.186 21.001 14.6964C20.8704 14.2067 20.55 13.7894 20.1108 13.5367V13.5367C19.6715 13.284 19.3511 12.8666 19.2206 12.3769C19.09 11.8873 19.16 11.3658 19.4151 10.928C19.581 10.6383 19.8211 10.3982 20.1108 10.2323V10.2323C21.0159 9.70289 21.3262 8.54349 20.8064 7.63277V7.63277V7.62361Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                                                                          <circle cx="12.1747" cy="11.8891" r="2.63616" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></circle>   
+                                                                          <circle cx="12.1747" cy="11.8891" r="2.63616" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></circle>
                                                                       </svg>
                                                                      </span>
                                                                 </a>
@@ -177,7 +177,7 @@ if (isset($_COOKIE['LinkCount'])) {
                                                             @endif
 
                                                         @if(file_exists(base_path("assets/favicon/icons/").localIcon($link->id)))<a style="float: right;" href="{{ route('clearIcon', $link->id ) }}"  data-bs-toggle="tooltip" data-bs-placement="top" aria-label="Add" data-bs-placement="top" data-original-title="Clear icon cache" class="float-right hvr-grow p-1 text-primary"><i style="-webkit-text-stroke:1px;padding-right:5px;" class="bi bi-arrow-repeat"></i></a>@endif
-                            
+
                                                     </div>
                                                 </div>
                                             </div>
@@ -187,21 +187,21 @@ if (isset($_COOKIE['LinkCount'])) {
                                         @endforeach
                                         @endif
                                     </div>
-                            
-                            
+
+
                                     <script type="text/javascript">
                                         const linksTableOrders = "{{ implode(' | ', $links->pluck('id')->toArray()) }}"
                                     </script>
                                 </div>
-                            
+
                                 <ul class="pagination justify-content-center">
                                     {!! $links ?? ''->links() !!}
                                 </ul>
-                            
+
                                 @if(count($links) > 3)<a class="btn btn-primary" href="{{ url('/studio/add-link') }}">{{__('messages.Add new Link')}}</a>@endif
                                 </div>
                             </section>
-                            
+
                             <section class='pre-right text-gray-400 pre-side'>
                                 <h3 class="card-header"><i class="bi bi-window-fullscreen" style="font-style:normal!important;"> {{__('messages.Preview')}}</i></h3>
                                     <div class='card-body p-0 p-md-3'>
@@ -209,7 +209,7 @@ if (isset($_COOKIE['LinkCount'])) {
                                      </div>
                             </section>
                             </div>
-                            
+
                             <br>
                             <section style="margin-left:-15px;margin-right:-15px;" style="width:100%!important;" class='pre-bottom text-gray-400 pre-side'>
                                 <h3 class="card-header"><i class="bi bi-window-fullscreen" style="font-style:normal!important;">{{__('messages.Preview')}}</i></h3>
@@ -217,16 +217,16 @@ if (isset($_COOKIE['LinkCount'])) {
                                             <center><iframe allowtransparency="true" id="frPreview2" style=" border-radius:0.25rem !important; background: #FFFFFF; min-height:600px; height:100%; width:100% !important;" class='w-100' src="{{ url('') }}/@<?= Auth::user()->littlelink_name ?>">{{__('messages.No compatible browser')}}</iframe></center>
                                      </div>
                             </section><br>
-                            
+
                             <section style="margin-left:-15px;margin-right:-15px;" class='text-gray-400'>
                             <a name="icons"></a>
                             <h3 class="mb-4 card-header"><i class="fa-solid fa-icons"></i> {{__('messages.Page Icons')}}</i></h3>
                             <div class="card-body p-0 p-md-3">
-                            
+
                             <form action="{{ route('editIcons') }}" enctype="multipart/form-data" method="post">
                                 @csrf
                                 <div class="form-group col-lg-8">
-                            
+
                                     @php
                                     if (!function_exists('iconLink')) {
                                         function iconLink($icon) {
@@ -242,7 +242,7 @@ if (isset($_COOKIE['LinkCount'])) {
                                             }
                                         }
                                     }
-                                    
+
                                     if (!function_exists('searchIcon')) {
                                         function searchIcon($icon) {
                                             $iconId = DB::table('links')
@@ -257,7 +257,7 @@ if (isset($_COOKIE['LinkCount'])) {
                                             }
                                         }
                                     }
-                                    
+
                                     if (!function_exists('iconclicks')) {
                                         function iconclicks($icon) {
                                             $iconClicks = searchIcon($icon);
@@ -269,7 +269,7 @@ if (isset($_COOKIE['LinkCount'])) {
                                             }
                                         }
                                     }
-                                    
+
                                     if (!function_exists('icon')) {
                                         function icon($name, $label) {
                                             echo '<div class="mb-3">
@@ -285,46 +285,46 @@ if (isset($_COOKIE['LinkCount'])) {
                                     }
                                     @endphp
                                     <style>input{border-top-right-radius: 0.25rem!important; border-bottom-right-radius: 0.25rem!important;}</style>
-                            
-                            
+
+
                                 {!!icon('mastodon', 'Mastodon')!!}
-                            
+
                                 {!!icon('instagram', 'Instagram')!!}
-                            
+
                                 {!!icon('twitter', 'Twitter')!!}
-                            
+
                                 {!!icon('facebook', 'Facebook')!!}
-                            
+
                                 {!!icon('github', 'GitHub')!!}
-                            
+
                                 {!!icon('twitch', 'Twitch')!!}
-                            
+
                                 {!!icon('linkedin', 'LinkedIn')!!}
-                            
+
                                 {!!icon('tiktok', 'TikTok')!!}
-                            
+
                                 {!!icon('discord', 'Discord')!!}
-                            
+
                                 {!!icon('youtube', 'YouTube')!!}
-                            
+
                                 {!!icon('snapchat', 'Snapchat')!!}
-                            
+
                                 {!!icon('reddit', 'Reddit')!!}
-                            
+
                                 {!!icon('pinterest', 'Pinterest')!!}
-                            
+
                                 {{-- {!!icon('telegram', 'Telegram')!!}
-                            
+
                                 {!!icon('whatsapp', 'WhatsApp')!!} --}}
-                            
-                            
+
+
                                 <button type="submit" class="mt-3 ml-3 btn btn-primary">{{__('messages.Save links')}}</button>
                             </form>
-                            
-                            
+
+
                             </div>
                             </section>
-    
+
                       </div>
                   </div>
                </div>
