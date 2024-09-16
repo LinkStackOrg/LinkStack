425eedfccc4f33a799a5d85e3660a39b20d7494e
Julian Prieber <julian.prieber@gmail.com>
2024-09-16 14:29:28 +0200

    Add link params

--- resources/views/components/pre-update.blade.php
+++ resources/views/components/pre-update.blade.php
@@ -4,7 +4,6 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\DB;
 use App\Models\Link;
 
-// Disable execution time limit
 set_time_limit(0);
 
 if(trim(file_get_contents(base_path("version.json"))) < '4.0.0'){
@@ -17,10 +16,8 @@ if(trim(file_get_contents(base_path("version.json"))) < '4.0.0'){
 } catch (Exception $e) {}
 }
 
-// Check version
 if (trim(file_get_contents(base_path("version.json"))) < '4.8.1') {
 
-// First, check if the 'type' and 'type_params' columns exist, and add them if they don't
 if (!Schema::hasColumn('links', 'type')) {
     Schema::table('links', function (Blueprint $table) {
         $table->string('type')->nullable();
@@ -33,42 +30,48 @@ if (!Schema::hasColumn('links', 'type_params')) {
     });
 }
 
-    // Get all links
     $links = Link::all();
 
     foreach ($links as $link) {
-        $type = null;
+    $type = null;
+    $params = false;
 
-        // Assign type based on button_id
-        switch ($link->button_id) {
-            case "1":
-            case "2":
-                $type = "link";
-                break;
-            case "43":
-                $type = "spacer";
-                break;
-            case "42":
-                $type = "heading";
-                break;
-            case "93":
-                $type = "text";
-                break;
-            case "44":
-                $type = "telephone";
-                break;
-            case "6":
-                $type = "email";
-                break;
-            case "96":
-                $type = "vcard";
-                break;
-        }
-
-        // Update the link if a type was assigned
-        if ($type !== null) {
-            $link->type = $type;
-            $link->save();
-        }
+    switch ($link->button_id) {
+        case "1":
+            $type = "link";
+            break;
+        case "2":
+            $type = "link";
+            break;
+        case "43":
+            $type = "spacer";
+            $params = true;
+            break;
+        case "42":
+            $type = "heading";
+            $params = true;
+            break;
+        case "93":
+            $type = "text";
+            $params = true;
+            break;
+        case "44":
+            $type = "telephone";
+            break;
+        case "6":
+            $type = "email";
+            break;
+        case "96":
+            $type = "vcard";
+            break;
     }
+
+    if ($type !== null) {
+        $link->type = $type;
+        if ($params === true) {
+            $link->type_params = json_encode(["custom_html" => true]);
+        }
+        $link->save();
+    }
+}
 }
