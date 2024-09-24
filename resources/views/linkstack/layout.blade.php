a05036f4ca6c5cd401dc7903fe85f3615668f060
Julian Prieber <julian.prieber@gmail.com>
2024-09-24 16:13:17 +0200

    Refactor layout and button display logic

--- resources/views/linkstack/layout.blade.php
+++ resources/views/linkstack/layout.blade.php
@@ -1,28 +1,15 @@
 <!DOCTYPE html>
 @include('layouts.lang')
 <head>
-   @include('linkstack.modules.meta')
-   
    @stack('linkstack-head')
-   @include('linkstack.modules.assets')
-   @foreach($information as $info)
    @stack('linkstack-head-end')
 </head>
 <body>
    @stack('linkstack-body-start')
-   @include('linkstack.modules.admin-bar')
-   @include('linkstack.modules.share-button')
-   @include('linkstack.modules.report-icon')
    <div class="container">
       <div class="row">
          <div class="column" style="margin-top: 5%">
-            @include('linkstack.elements.avatar')
-            @include('linkstack.elements.heading')
-            @include('linkstack.elements.bio')
-            @include('linkstack.elements.icons')
-            @endforeach
-            @yield('content')
-            @include('linkstack.modules.footer')
+            @stack('linkstack-content')
          </div>
       </div>
    </div>
--- resources/views/linkstack/linkstack.blade.php
+++ resources/views/linkstack/linkstack.blade.php
@@ -1,7 +1,32 @@
 @extends('linkstack.layout')
+
 @section('content')
-    @push('linkstack-head-end')
-        @include('linkstack.modules.theme')
+    @push('linkstack-head')
+        @include('linkstack.modules.meta')
+        @include('linkstack.modules.assets')
     @endpush
-@include('linkstack.elements.buttons')
+
+    @push('linkstack-head-end')
+        @foreach($information as $info)
+            @include('linkstack.modules.theme')
+        @endforeach
+    @endpush
+
+    @push('linkstack-body-start')
+        @include('linkstack.modules.admin-bar')
+        @include('linkstack.modules.share-button')
+        @include('linkstack.modules.report-icon')
+    @endpush
+
+    @push('linkstack-content')
+        @foreach($information as $info)
+            @include('linkstack.elements.avatar')
+            @include('linkstack.elements.heading')
+            @include('linkstack.elements.bio')
+        @endforeach
+        @include('linkstack.elements.icons')
+        @include('linkstack.elements.buttons')
+        @yield('content')
+        @include('linkstack.modules.footer')
+    @endpush
 @endsection
