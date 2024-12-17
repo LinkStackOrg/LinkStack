fec1cd0dd52b0537a9a151907e758ec35a9ef18a
Julian Prieber <julian.prieber@gmail.com>
2024-12-17 19:09:26 +0100

    Disable automatic asset injection and add core Livewire assets to user panel

--- config/livewire-tables.php
+++ config/livewire-tables.php
@@ -5,4 +5,6 @@ return [
      * Options: tailwind | bootstrap-4 | bootstrap-5.
      */
     'theme' => 'bootstrap-5',
+    'inject_core_assets_enabled' => false,
+    'inject_third_party_assets_enabled' => false,
 ];
--- resources/views/panel/users.blade.php
+++ resources/views/panel/users.blade.php
@@ -127,10 +127,12 @@
 
     @push('sidebar-stylesheets')
         @livewireStyles
+        <link rel="stylesheet" href="{{ asset('assets/vendor/livewire/core.min.css') }}" />
     @endpush
 
     @push('sidebar-scripts')
         <script src="{{ asset('assets/vendor/livewire/livewire.js') }}" data-update-uri="/livewire/update" data-navigate-once="true"></script>
+        <script src="{{ asset('assets/vendor/livewire/core.min.js') }}"></script>
         <script src="{{ asset('assets/external-dependencies/sweetalert2.min.js') }}"></script>
         <script type="text/javascript">
             // Function to confirm and delete users
