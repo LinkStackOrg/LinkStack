667aa351fb7d4f8b9301f17eb417ea8e26d85ce7
Julian Prieber <julian.prieber@gmail.com>
2025-12-18 23:21:53 +0100

    Revert failed test system

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -4,56 +4,50 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
-use App\Models\User;
+use App\Providers\RouteServiceProvider;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use GeoSot\EnvEditor\Facades\EnvEditor;
 
 class AuthenticatedSessionController extends Controller
 {
     /**
      * Display the login view.
+     *
+     * @return \Illuminate\View\View
      */
     public function create()
     {
-        if (EnvEditor::keyExists('UPDATER_USER_ID')) {
-            $userId = EnvEditor::getKey('UPDATER_USER_ID');
-
-            // Optional: one-time use
-            EnvEditor::deleteKey('UPDATER_USER_ID');
-
-            $user = User::find($userId);
-
-            if ($user) {
-                Auth::login($user);
-                request()->session()->regenerate();
-
-                return redirect('/update?finishing');
-            }
-        }
-
         return view('auth.login');
     }
 
     /**
      * Handle an incoming authentication request.
+     *
+     * @param  \App\Http\Requests\Auth\LoginRequest  $request
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function store(LoginRequest $request)
     {
+
         $request->authenticate();
+
         $request->session()->regenerate();
 
         return redirect('/dashboard');
-    }
 
+    }
     /**
      * Destroy an authenticated session.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function destroy(Request $request)
     {
         Auth::guard('web')->logout();
 
         $request->session()->invalidate();
+
         $request->session()->regenerateToken();
 
         return redirect('/');
